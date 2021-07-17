dc24f598e0944f9cb0c440b76152a01dabb36d77
Martinus Davin Herell <davin_herell@protonmail.com>
2021-07-17 12:54:28 +0700

    add Explodable script

---
+++ FireAndExplosionProject/Assets/Scripts/Explodable.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explodable : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    public float delay = 3f;
+    public float blastRadius = 5f;
+    public float explosionForce = 700f;
+    public GameObject explosionEffect;
+    float countDown;
+    bool startExplosion = false;
+    bool hasExploded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startExplosion)
+        {
+            countDown -= Time.deltaTime;
+
+            if (countDown <= 0f && !hasExploded)
+            {
+                Explode();
+                hasExploded = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "ExplosionCatalyst")
+        {
+            startExplosion = true;
+        };
+    }
+
+    void Explode()
+    {
+        // Show explosion effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        // Instantiate the destroyed version of current object
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+
+        // Get nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            // Add force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, blastRadius);
+            }
+        }
+
+
+        // Remove the exploded object
+        Destroy(gameObject);
+    }
+}
